f071acde7e55a742e6643e5cd6459f98165ac7de
emlyle <emlyle@users.noreply.github.com>
2019-07-03 18:33:44 -0500

    Assignment 2 Refactoring: Implement minion card logic in a separate function called executeMinionCard; within executeMinionCard combine nested if statements using AND operator

--- dominion/dominion.c
+++ dominion/dominion.c
@@ -739,6 +739,59 @@ int executeBaronCard(int choice1, struct gameState *state, int handPos, int curr
 }
 
 
+//TODO: Add function definition 
+int executeMinionCard(int choice1, int choice2, struct gameState *state, int handPos, int currentPlayer)
+{
+	//+1 action
+	state->numActions++;
+
+	//discard card from hand
+	discardCard(handPos, currentPlayer, state, handPos, 0);
+
+	if (choice1)		//+2 coins
+	{
+		state->coins = state->coins + 2;
+	}
+
+	else if (choice2)		//discard hand, redraw 4, other players with 5+ cards discard hand and draw 4
+	{
+		//discard hand
+		while (numHandCards(state) > 0)
+		{
+			discardCard(handPos, currentPlayer, state, 0);
+		}
+
+		//draw 4
+		int i; 
+		for (i = 0; i < 4; i++)
+		{
+			drawCard(currentPlayer, state);
+		}
+
+		//other players discard hand and redraw if hand size > 4
+		for (i = 0; i < state->numPlayers; i++)
+		{
+			if (i != currentPlayer && state->handCount[i] > 4) //***Refactor: Combine nested if statements with AND operator
+			{
+				//discard hand
+				while (state->handCount[i] > 0)
+				{
+					discardCard(handPos, i, state, 0);
+				}
+
+				//draw 4
+				int j; 
+				for (j = 0; j < 4; j++)
+				{
+					drawCard(i, state);
+				}
+			}
+		}
+	}
+	return 0;
+}
+
+
 
 int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState *state, int handPos, int *bonus)
 {
@@ -865,7 +918,7 @@ int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState
       return -1;
 			
     case mine:
-		//Refactoring for Assignment 2: Implement mine card logic into separate function called executeMineCard
+		//Refactoring for Assignment 2: Implement mine card logic in separate function called executeMineCard
 		return executeMineCard(choice1, choice2, state, handPos, currentPlayer); 
 
 			
@@ -918,7 +971,7 @@ int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState
       return 0;
 		
     case baron:
-		//Refactoring for Assignment 2: Implement baron card logic into separate function called executeBaronCard
+		//Refactoring for Assignment 2: Implement baron card logic in separate function called executeBaronCard
 		return executeBaronCard(choice1, state, handPos, currentPlayer);
 
     case great_hall:
@@ -933,55 +986,9 @@ int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState
       return 0;
 		
     case minion:
-      //+1 action
-      state->numActions++;
-			
-      //discard card from hand
-      discardCard(handPos, currentPlayer, state, 0);
-			
-      if (choice1)		//+2 coins
-	{
-	  state->coins = state->coins + 2;
-	}
-			
-      else if (choice2)		//discard hand, redraw 4, other players with 5+ cards discard hand and draw 4
-	{
-	  //discard hand
-	  while(numHandCards(state) > 0)
-	    {
-	      discardCard(handPos, currentPlayer, state, 0);
-	    }
-				
-	  //draw 4
-	  for (i = 0; i < 4; i++)
-	    {
-	      drawCard(currentPlayer, state);
-	    }
-				
-	  //other players discard hand and redraw if hand size > 4
-	  for (i = 0; i < state->numPlayers; i++)
-	    {
-	      if (i != currentPlayer)
-		{
-		  if ( state->handCount[i] > 4 )
-		    {
-		      //discard hand
-		      while( state->handCount[i] > 0 )
-			{
-			  discardCard(handPos, i, state, 0);
-			}
-							
-		      //draw 4
-		      for (j = 0; j < 4; j++)
-			{
-			  drawCard(i, state);
-			}
-		    }
-		}
-	    }
-				
-	}
-      return 0;
+		//Refactoring for Assignment 2: Implement minion card logic in separate function called executeMinionCard
+		return executeMinionCard(choice1, choice2, state, handPos, currentPlayer);
+
 		
     case steward:
       if (choice1 == 1)
