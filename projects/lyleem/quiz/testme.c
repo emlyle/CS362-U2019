c0cfe38ca9552cde00315279cb1b0bf72ee4e1da
emlyle <emlyle@users.noreply.github.com>
2019-07-23 17:28:55 -0500

    Random Quiz: Clean up comments in testme.c

--- projects/lyleem/quiz/testme.c
+++ projects/lyleem/quiz/testme.c
@@ -6,23 +6,23 @@
 
 char inputChar()
 {
-    // TODO: rewrite this function
+	//This function randomly selects and returns one character from the charPool array 
     char charPool[9] = {'[', '(', '{', ' ', 'a', 'x', '}', ')', ']'}; 
     int randPos = rand() % 9;
-    //printf("randChar = %c, randPos = %d\n", charPool[randPos], randPos); 
     return charPool[randPos];
 }
 
 char *inputString()
 {
+	//This function randomly reorders and returns the characters r, e, s, e, t in a string 
     int randPos, i, j; 
-    // TODO: rewrite this function
     char string[6] = "12345";  
     char charPool[5] = {'r', 'e', 's', 'e', 't'};
     int strLen = 5; 
     for (i = 0; i < 5; i++) {
        randPos = rand() % strLen--; 
        string[i] = charPool[randPos];
+	   //remove selected element and shift remaining elements to the left in charPool array
        for (j = randPos; j < strLen; j++) {
 		charPool[j] = charPool[j + 1]; 
        }  
@@ -37,27 +37,8 @@ void testme()
   char c;
   int state = 0;
 
-//TEMP:
-  //int i = 0; 
-  //for (i = 0; i < 20; i++) {
-  //   c = inputChar(); 
-  //}
-  //int myTempCtr = 0;
-
-
-
   while (1)
   { 
-    //TEMP
-    //myTempCtr++; 
-    //if (myTempCtr == 500) {
-	//printf("my exit\n"); 
-	//break; 
-	//exit(200); 
-    //}
-    //printf("%s\n", s); 
-
-
     tcCount++;
     c = inputChar();
     s = inputString();
@@ -71,11 +52,7 @@ void testme()
     if (c == 'x' && state == 5) state = 6;
     if (c == '}' && state == 6) state = 7;
     if (c == ')' && state == 7) state = 8;
-    if (c == ']' && state == 8) {
-	state = 9;
-	//printf("***finished last char if statement\n"); 
-	//break; 
-    }
+    if (c == ']' && state == 8) state = 9; 
     if (s[0] == 'r' && s[1] == 'e'
        && s[2] == 's' && s[3] == 'e'
        && s[4] == 't' && s[5] == '\0'
