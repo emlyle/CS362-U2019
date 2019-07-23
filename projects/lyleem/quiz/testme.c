bf347cfc74b80082c09bbe9dab397aa0db5eb6a8
Emily Lyle <lyleem@flip3.engr.oregonstate.edu>
2019-07-22 20:22:09 -0700

    Random Quiz: Add test methods to testme.c; edit makefile

--- projects/lyleem/quiz/testme.c
+++ projects/lyleem/quiz/testme.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<math.h>
 #include<string.h>
 #include<stdlib.h>
 #include<time.h>
@@ -6,14 +7,27 @@
 char inputChar()
 {
     // TODO: rewrite this function
-	char randChar = '[' + (random() % 35);
-    return randChar;
+    char charPool[9] = {'[', '(', '{', ' ', 'a', 'x', '}', ')', ']'}; 
+    int randPos = rand() % 9;
+    //printf("randChar = %c, randPos = %d\n", charPool[randPos], randPos); 
+    return charPool[randPos];
 }
 
 char *inputString()
 {
+    int randPos, i, j; 
     // TODO: rewrite this function
-    return "reset";
+    char string[6] = "12345";  
+    char charPool[5] = {'r', 'e', 's', 'e', 't'};
+    int strLen = 5; 
+    for (i = 0; i < 5; i++) {
+       randPos = rand() % strLen--; 
+       string[i] = charPool[randPos];
+       for (j = randPos; j < strLen; j++) {
+		charPool[j] = charPool[j + 1]; 
+       }  
+    }
+    return string;
 }
 
 void testme()
@@ -22,8 +36,28 @@ void testme()
   char *s;
   char c;
   int state = 0;
+
+//TEMP:
+  //int i = 0; 
+  //for (i = 0; i < 20; i++) {
+  //   c = inputChar(); 
+  //}
+  //int myTempCtr = 0;
+
+
+
   while (1)
-  {
+  { 
+    //TEMP
+    //myTempCtr++; 
+    //if (myTempCtr == 500) {
+	//printf("my exit\n"); 
+	//break; 
+	//exit(200); 
+    //}
+    //printf("%s\n", s); 
+
+
     tcCount++;
     c = inputChar();
     s = inputString();
@@ -37,16 +71,23 @@ void testme()
     if (c == 'x' && state == 5) state = 6;
     if (c == '}' && state == 6) state = 7;
     if (c == ')' && state == 7) state = 8;
-    if (c == ']' && state == 8) state = 9;
+    if (c == ']' && state == 8) {
+	state = 9;
+	//printf("***finished last char if statement\n"); 
+	//break; 
+    }
     if (s[0] == 'r' && s[1] == 'e'
        && s[2] == 's' && s[3] == 'e'
        && s[4] == 't' && s[5] == '\0'
        && state == 9)
     {
       printf("error ");
-      exit(200);
+      break; 
+      //exit(200);
     }
+
   }
+
 }
 
 
